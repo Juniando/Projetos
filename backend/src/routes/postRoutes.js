2937c8d7918ab88a396d7f6eb0fd7de0b416dea6
thsouzaAc <thiagosouzabrito8@gmail.com>
2025-09-08 19:38:50 -0300

    feat: Adiconando Rotas Delete e Listagem por Id

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import {createPost, getAllPosts} from '../controllers/postController.js';
-import validatePost from '../middlewares/validatePostMiddlewares.js';
-
+import {createPost, deletePost, getAllPosts, getPost} from '../controllers/postController.js';
+import validatePost from '../middlewares/validatePostMiddleware.js';
+import checkPostExist from '../middlewares/checkPostExistsMiddleware.js'
 const postRouter = express.Router();
 
 postRouter.post('/createPost', validatePost, createPost);
 postRouter.get('/posts', getAllPosts);
+postRouter.delete('/deletePost/:id', checkPostExist, deletePost);
+postRouter.get('/post/:id',checkPostExist, getPost);
 
 export default postRouter;
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {createUser, getAllUsers} from '../controllers/userController.js';
+import {createUser, deleteUser, getAllUsers} from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 
 userRouter.post('/createUser', createUser);
 userRouter.get('/users', getAllUsers);
+userRouter.delete('/deleteUser/:id', deleteUser);
 
 export default userRouter;
